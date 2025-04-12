ededae43c76b8679af74b1664afaf02072d1dda5
sanjiv576 <shresthasanjiv576@gmail.com>
2025-04-12 14:51:23 +0545

    update

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:usersbook/view/home_screen.dart';
+import 'features/presentation/view/home_screen.dart';
 
-class AppScreen extends ConsumerWidget {
-  const AppScreen({super.key});
+class AppView extends ConsumerWidget {
+  const AppView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-      },
+      routes: {'/': (context) => HomeScreen()},
     );
   }
 }
